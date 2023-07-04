c433dd1269d086d19626ad5ea0e4b4d06bfb02dc
Mohd Zaid <mohdzaid@Mohds-MacBook-Air.local>
2023-07-04 19:43:57 +0530

    Sign/Login Pages Added || Routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import SignIn from './Pages/login';
+import { createRoot } from "react-dom/client";
+import {
+  BrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+  Router, Routes
+} from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<SignIn />}/>
+            <Route path="/" element={<div><h1>Welcome</h1></div>} />
+
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
