76e5a1baca0d4b175a51098b73ce319316cb152d
Mohd Zaid <mohdzaid@Mohds-MacBook-Air.local>
2023-07-05 02:56:05 +0530

    Card component/ Landing UI/Navbar fixes

--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -1,11 +1,48 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
+import Background from './backg.jpg'
+import { FaExternalLinkAlt } from 'react-icons/fa'
+import Card from '../../components/Card/Card'
 
 export default function () {
-  return (
-    <div>
-        <Navbar/>
-        
-    </div>
-  )
+    return (
+        <div>
+            <Navbar />
+            <div className='bg-green-900 h-[60vh] relative'>
+
+                <div className='p-20'>
+                    <div>
+                        <div className='text-7xl text-white'>A perfect place for <br />Pictures enthusiast.</div>
+                        <div className='w-4/12 text-sm text-white font-thin my-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus nesciunt, explicabo provident et esse minima nobis deserunt nostrum, excepturi est perferendis ipsa id illum! Perferendis dolore consequuntur labore qui eum.</div>
+                        <div>
+                            <button className='text-white flex items-center gap-1 text-sm font-light hover:underline'>
+                                <div>Learn More</div>
+                                <FaExternalLinkAlt />
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div className='px-20 my-10'>
+                    <div className='text-3xl font-semibold text-green-900 mb-5'>Todays Best Deals For You!</div>
+                    <div className='flex gap-5'>
+                        <Card />
+                        <Card />
+                        <Card />
+                    </div>
+                </div>
+                <div className='px-20 my-20'>
+                    <div className='text-3xl font-semibold text-green-900 mb-5'>Best Selling Pictures</div>
+                    <div className='flex gap-5'>
+                        <Card />
+                        <Card />
+                        <Card />
+                        <Card />
+                    </div>
+                </div>
+
+                <div></div>
+
+            </div>
+        </div>
+    )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import SignIn from '../../Pages/Login/login'
 import  {FaShoppingCart}  from "react-icons/fa";
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
     return (
         <div className='h-[10vh]  flex justify-center'>
-            <div className='flex items-center justify-between h-full w-[95%] '>
+            <div className='relative flex items-center justify-between h-full w-[95%] '>
                 <div className='bg-white flex items-center gap-3'>
                     <FaShoppingCart className='text-green-900 text-3xl'/>
-                    <div className='text-3xl text-green-800'>Shopping</div>
+                    <div className='text-3xl text-green-800 font-semibold'>Snapshoot</div>
                 </div>
                 <div className='bg-white flex gap-3'>
-                    <button className='bg-green-800 border border-green-800  text-white py-1.5 rounded px-4 hover:text-green-800 hover:bg-white'>Log In</button>
-                    <button className='bg-white border border-green-800 text-green-800 py-1.5 rounded px-4 hover:bg-green-800 hover:text-white'>Sign up</button>
+                    <Link to='/login'><button className='bg-green-900 border border-green-900  text-white py-2 rounded px-6 hover:text-green-900 hover:bg-white text-sm'>Log In</button></Link>
+                    <Link to='/login'>
+                    <button className='bg-white border border-green-900 text-green-900 py-2 rounded px-6 hover:bg-green-900 hover:text-white text-sm'>Sign up</button></Link>
                 </div>
             </div>
 
